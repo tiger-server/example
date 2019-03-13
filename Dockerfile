03f9cecae76e6a66199694e41eab6f3a490b13e7
KimmyLeo <kenpusney@gmail.com>
2019-03-13 10:26:30 +0000

    update build status

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM node:10
-COPY ./ ./
-RUN npm install
-CMD [ "node", "mate.js" ]
+ADD package.json /tmp/package.json
+RUN cd /tmp && npm install
+RUN mkdir -p /opt/app && cp -a /tmp/node_modules /opt/app/
+COPY ./ /opt/app/
+CMD [ "node", "/opt/app/mate.js" ]
--- mate.js
+++ mate.js
@@ -1,10 +1,13 @@
 const config = require("./config")
 const tiger = require("tiger-server")(config)
 const luxon = require("luxon")
-
 const { mail } = require("tiger-server/protocols")
 
 tiger.use(mail)
+
+tiger.define("version", ["http:/version", function (tiger, state, {req, res}) {
+  res.send({ "version": 1 });
+}]);
 
 tiger.define("minutes-timer", ["cron:0 0/30 * * * *", function (tiger) {
   tiger.notify("zmq:timer", { time: luxon.DateTime.local().setZone('Asia/Shanghai').startOf('minute').toISO() })
@@ -13,7 +16,6 @@ tiger.define("minutes-timer", ["cron:0 0/30 * * * *", function (tiger) {
 
 tiger.define("timer-notification", ["zmq:timer", function(tiger, state, { time }) {
   const text = `贴身小伙伴为您报时: ${time}`;
-
   tiger.notify(config.mail.channel, { subject: text, text: text, html: `<p>${text}</p>`  })
 }]);
 
