7b10125907a2ad46745173409a3a44610c62b3a8
Kimmy <kenpusney@gmail.com>
2019-11-17 00:52:34 +0800

    upgrade tiger-server

--- mate.js
+++ mate.js
@@ -1,22 +1,38 @@
 const config = require("./config")
-const tiger = require("tiger-server")(config)
+const { Tiger } = require("tiger-server")
 const luxon = require("luxon")
-const { mail } = require("tiger-server/protocols")
+const { mail, http, cron, zmq } = require("tiger-server/lib/core")
+
+const tiger = new Tiger(config)
 
 tiger.use(mail)
+tiger.use(http)
+tiger.use(cron)
+tiger.use(zmq)
 
-tiger.define("version", ["http:/version", function (tiger, state, {req, res}) {
-  res.send({ "version": 1 });
-}]);
+tiger.define({
+  id: "version",
+  target: "http:/version", 
+  process: function (state, {req, res}) {
+    res.send({ "version": 1 });
+  }
+});
 
-tiger.define("minutes-timer", ["cron:0 0/30 * * * *", function (tiger) {
-  tiger.notify("zmq:timer", { time: luxon.DateTime.local().setZone('Asia/Shanghai').startOf('minute').toISO() })
-}]);
+tiger.define({
+  id: "minutes-timer", target: "cron:0 0/30 * * * *", 
+  process: function () {
+    this.notify("zmq:timer", { time: luxon.DateTime.local().setZone('Asia/Shanghai').startOf('minute').toISO() })
+  }
+});
 
 
-tiger.define("timer-notification", ["zmq:timer", function(tiger, state, { time }) {
-  const text = `贴身小伙伴为您报时: ${time}`;
-  tiger.notify(config.mail.channel, { subject: text, text: text, html: `<p>${text}</p>`  })
-}]);
+tiger.define({
+  id: "timer-notification", 
+  target: "zmq:timer", 
+  process: function(state, { time }) {
+    const text = `贴身小伙伴为您报时: ${time}`;
+    this.notify(config.mail.channel, { subject: text, text: text, html: `<p>${text}</p>`  })
+  }
+});
 
 tiger.serve();
